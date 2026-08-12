32c368b65b331f9d03d48efaeafc4a4a4f7b7e78
Rahul Shukla <rahulshukla@e-sutra.com>
2026-08-12 21:17:23 +0530

    implement hosting images

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,6 +4,8 @@ import { ArrowRight, ArrowUpRight, Clock, Layers } from "lucide-react";
 import Button from "../../components/ui/Button";
 import Upload from "../../components/Upload";
 import { useNavigate } from "react-router";
+import { useState } from "react";
+import {createProject} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,9 +17,35 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   const navigate = useNavigate();
 
+  const [projects, setProjects] = useState<DesignItem[]>([]);
+
   const handleUploadComplete = async (base64Image: string) => {
     const newId = Date.now().toString();
-    navigate(`/visualizer/${newId}`);
+    const name = `Residence ${newId}`;
+
+    const newItem = {
+      id: newId,
+      name,
+      sourceImage: base64Image,
+      renderedImage: undefined,
+      timestamp: Date.now()
+    };
+
+    const saved = await createProject({ item: newItem, visibility: "private" });
+    
+    if (!saved) {
+      console.error("Failed to create project");
+      return false;
+    }
+    setProjects((prev) => [newItem, ...prev]);
+
+    navigate(`/visualizer/${newId}`, {
+      state: {
+        initialImage: saved.sourceImage,
+        initialRendered: saved.renderedImage || null,
+        name,
+      }
+    });
     return true;
   }
 
@@ -73,31 +101,33 @@ export default function Home() {
           </div>
 
           <div className="projects-grid">
-            <div className="project-card group">
-              <div className="preview">
-                <img src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png" alt="Project" />
+            {projects.map(({ id, name, renderedImage, sourceImage, timestamp, }) => (
+              <div className="project-card group">
+                <div className="preview">
+                  <img src={renderedImage || sourceImage} alt="Project" />
 
-                <div className="badge">
-                  <span>Community</span>
-                </div>
-              </div>
-              
-              <div className="card-body">
-                <div>
-                  <h3>Project Avatar</h3>
-
-                  <div className="meta">
-                    <Clock size={12} />
-                    <span>{new Date("01.01.2027").toLocaleDateString()}</span>
-                    <span>By RS</span>
+                  <div className="badge">
+                    <span>Community</span>
                   </div>
                 </div>
+                
+                <div className="card-body">
+                  <div>
+                    <h3>{name}</h3>
 
-                <div className="arrow">
-                  <ArrowUpRight size={18} />
+                    <div className="meta">
+                      <Clock size={12} />
+                      <span>{new Date(timestamp).toLocaleDateString()}</span>
+                      <span>By RS</span>
+                    </div>
+                  </div>
+
+                  <div className="arrow">
+                    <ArrowUpRight size={18} />
+                  </div>
                 </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
